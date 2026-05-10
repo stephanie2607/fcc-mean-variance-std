5b072e06745db698f81dc464d10f9b37730c1fa1
stephanie2607 <stephparabis@gmail.com>
2026-05-10 15:26:52 +0300

    Complétion du projet Mean-Var-Std

--- var_mean_std.py
+++ var_mean_std.py
@@ -43,7 +43,6 @@ def calculate(list):
     return calculations
 
 if __name__ == "__main__":
-    # Exemple d'utilisation
     input_list = [0,1,2,3,4,5,6,7,8]
     result = calculate(input_list)
     print(result)
